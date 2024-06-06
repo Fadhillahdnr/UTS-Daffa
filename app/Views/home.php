be4e1c5fe1602d983a8092926f24f67d634ddf8f
Fadhillahdnr <m.fadhillah1218@gmail.com>
2024-06-07 02:14:17 +0700

    push

---
+++ app/Views/home.php
@@ -0,0 +1,281 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <!-- Site Metas -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+
+  <title>Daffa M Rosyadi</title>
+
+  <!-- slider stylesheet -->
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url("cssku")?>/css/bootstrap.css" />
+
+  <!-- Custom styles for this template -->
+  <link href=" <?php echo base_url("cssku")?>/css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="<?php echo base_url("cssku")?>/css/responsive.css" rel="stylesheet" />
+</head>
+
+<body>
+  <div class="hero_area">
+    <!-- header section strats -->
+    <header class="header_section">
+      <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg custom_nav-container d-block">
+          <div class="main_nav_menu">
+            <div class="fk_width">
+              <div class="custom_menu-btn">
+                <button onclick="openNav()">
+                  <span class="s-1"> </span>
+                  <span class="s-2"> </span>
+                  <span class="s-3"> </span>
+                </button>
+              </div>
+              <div id="myNav" class="overlay">
+                <div class="overlay-content">
+                  <a class="" href="/">Home <span class="sr-only">(current)</span></a>
+                  <a class="" href="/gallery">Gallery </a>
+                </div>
+              </div>
+            </div>
+            <a class="navbar-brand" href="/">
+              <span>
+                Daffa Miftahurrohman Rosyadi
+              </span>
+            </a>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <!-- end header section -->
+
+    <!-- slider section -->
+    <section class=" slider_section position-relative">
+      <div class="container-fluid">
+        <div class="row slider-row">
+          <div class="col-lg-3 offset-lg-1">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <div class="detail-box">
+                    <h2>
+                      About Me
+                    </h2>
+                    <p>
+                    My name is Daffa Miftahurrohman Rosyadi, I am a student at the University of Muhammadiyah Sukabumi majoring in Informatics Engineering. In addition, I also work at PT.Rahayu Sawit Sejahtera as a director. I have special skills in the IT field, one of which is an expert in using Microsoft Office. 
+                    </p>
+                    <p>
+                    I have two businesses, namely the Palm kernel shell business and the Home Stay Villa which I manage with my team.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="carousel_control-box">
+                <div class="carousel_btn-container">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-8 px-0">
+            <div class="img-box">
+              <img src="<?php echo base_url("cssku")?>/images/slider-img.jpg" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- end slider section -->
+  </div>
+
+  <!-- about section -->
+
+    <!-- <section class="about_section layout_padding">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-5">
+            <div class="detail-box">
+              <div class="heading_container">
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="img-box">
+        <img src="<?php echo base_url("cssku")?>/images/about-img.png" class="about-img" alt="" />
+      </div>
+    </section> -->
+
+  <!-- end about section -->
+
+  <!-- gallery section -->
+  <section class="gallery_section layout_padding-top">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-5 ml-auto">
+          <div class="heading_container">
+            <h2>
+              My Gallery
+            </h2>
+            <p>
+            Life is full of adventure. There is no such thing as a clear path.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="gallery_container">
+      <div class="gallery_bg">
+        <img src="<?php echo base_url("cssku")?>/images/gallery-bg.png" alt="" />
+      </div>
+      <div class="container">
+        <div class="gallery_box">
+          <div class="box b1">
+            <div class="img-box">
+              <img src="<?php echo base_url("cssku")?>/images/gallery-1.jpg" alt="" />
+              <h5>
+              </h5> 
+            </div>
+          </div>
+          <div class="box b2">
+            <div class="img-box">
+              <img src="<?php echo base_url("cssku")?>/images/gallery-2.jpg" alt="" />
+              <h5>
+              </h5>
+            </div>
+            <div class="img-box">
+              <img src="<?php echo base_url("cssku")?>/images/gallery-3.png" alt="" />
+              <h5>
+              </h5>
+            </div>
+          </div>
+          <div class="box b3">
+            <div class="img-box">
+              <img src="<?php echo base_url("cssku")?>/images/gallery-4.jpg" alt="" />
+              <h5>
+              </h5>
+            </div>
+            <div class="img-box">
+              <img src="<?php echo base_url("cssku")?>/images/gallery-5.jpg" alt="" />
+              <h5>
+              </h5>
+            </div>
+          </div>
+          <div class="box b4">
+            <div class="img-box">
+              <img src="<?php echo base_url("cssku")?>/images/gallery-6.jpg" alt="" />
+              <h5>
+              </h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- end gallery section -->
+
+  <!-- info section -->
+
+  <section class="info_section ">
+    <div class="info_container layout_padding-top">
+      <div class="container">
+        <div class="heading_container">
+          <h2>
+            Hubungi Saya
+          </h2>
+        </div>
+        <div class="info_main">
+          <div class="row">
+            <div class="col-md-4 col-lg-3">
+              <div class="info_contact ">
+                <a href="#" class="link-box">
+                  <div class="img-box">
+                    <img src="<?php echo base_url("cssku")?>/images/location.png" alt="" />
+                  </div>
+                  <div class="detail-box">
+                    <h6>
+                      Sukabumi
+                    </h6>
+                  </div>
+                </a>
+                <a href="#" class="link-box">
+                  <div class="img-box">
+                    <img src="<?php echo base_url("cssku")?>/images/mail.png" alt="" />
+                  </div>
+                  <div class="detail-box">
+                    <h6>
+                    d4ffa.crb@gmail.com
+                    </h6>
+                  </div>
+                </a>
+                <a href="https://wa.me/6287728111128" class="link-box">
+                  <div class="img-box">
+                    <img src="<?php echo base_url("cssku")?>/images/call.png" alt="" />
+                  </div>
+                  <div class="detail-box">
+                    <h6>
+                      Whatsapp +62 877 2811 1128
+                    </h6>
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div class="col-md-2 col-lg-3">
+              <div class="info_link-box">
+                <ul>
+                  <li class=" ">
+                    <a class="" href="/">Home <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="">
+                    <a class="" href="/about">About </a>
+                  </li>
+                  <li class="">
+                    <a class="" href="/gallery">Gallery </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-md-6 ">
+              <div class="social_box">
+                <a href="#">
+                  <img src="<?php echo base_url("cssku")?>/images/facebook.png" alt="" />
+                </a>
+                <a href="#">
+                  <img src="<?php echo base_url("cssku")?>/images/twitter.png" alt="" />
+                </a>
+                <a href="#">
+                  <img src="<?php echo base_url("cssku")?>/images/linkedin.png" alt="" />
+                </a>
+                <a href="#">
+                  <img src="<?php echo base_url("cssku")?>/images/instagram.png" alt="" />
+                </a>
+                <a href="#">
+                  <img src="<?php echo base_url("cssku")?>/images/youtube.png" alt="" />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- end info section -->
+
+  <script src="<?php echo base_url("cssku")?>/js/jquery-3.4.1.min.js"></script>
+  <script src="<?php echo base_url("cssku")?>/js/bootstrap.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+  <script src="<?php echo base_url("cssku")?>/js/custom.js"></script>
+</body>
+
+</html>
